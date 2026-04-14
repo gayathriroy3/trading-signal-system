8d76bd2884e87c6b96666ab6f6c7c5e677433a6d
Gayathri Roy <gayathriroy3@gmail.com>
2026-04-14 18:31:36 +0530

    refresh data on selected trade date

--- trading-system/src/data/refresh_data.py
+++ trading-system/src/data/refresh_data.py
@@ -10,4 +10,4 @@ def load_predict(selected_date):
     end_date_for_yf = pd.Timestamp(selected_date) + timedelta(days=1)
     start_date_for_yf = end_date_for_yf - timedelta(days=7) 
     nifty_predict = yf.download(NIFTY, start=start_date_for_yf, end=end_date_for_yf, interval="5m")
-    return nifty_predict,start_date_for_yf,end_date_for_yf,selected_date
+    return nifty_predict,start_date_for_yf,end_date_for_yf
